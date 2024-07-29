21dcde4ff2941d3575601effcf26f775f0336918
github-action-benchmark <github@users.noreply.github.com>
2024-07-29 10:24:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 9b64ebe0611e53c098fa7f0b0108a262af59eb42

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722248361515,
+  "lastUpdate": 1722248656784,
   "repoUrl": "https://github.com/amagnan/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -120,6 +120,72 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
             "value": 3.1483812904615833,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "a.magnan@imperial.ac.uk",
+            "name": "amagnan",
+            "username": "amagnan"
+          },
+          "committer": {
+            "email": "a.magnan@imperial.ac.uk",
+            "name": "amagnan",
+            "username": "amagnan"
+          },
+          "distinct": true,
+          "id": "9b64ebe0611e53c098fa7f0b0108a262af59eb42",
+          "message": "readd iDM config files",
+          "timestamp": "2024-07-29T12:08:47+02:00",
+          "tree_id": "f17a880ef6113e8b384d610215d9f23c8527ca45",
+          "url": "https://github.com/amagnan/FCCAnalyses/commit/9b64ebe0611e53c098fa7f0b0108a262af59eb42"
+        },
+        "date": 1722248655554,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/weaver/analysis_inference.py",
+            "value": 37.99383878707886,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/weaver/analysis_inference.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 11.588257074356079,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts/analysis_example.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 14.627787828445435,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 16.89140009880066,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test/jet_constituents.py",
+            "value": 12.719081401824951,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test/jet_constituents.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 32.46615028381348,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
           }
         ]
       }
