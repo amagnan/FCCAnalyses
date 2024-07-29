66332086b4fa477b8ca20d99c15ec1318d45c30d
github-action-benchmark <github@users.noreply.github.com>
2024-07-29 10:19:20 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ecf61353dfab4cb2e95fd7a8f8e7f12757efb7e0

---
+++ dev/bench/data.js
@@ -0,0 +1,74 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1722248360178,
+  "repoUrl": "https://github.com/amagnan/FCCAnalyses",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ecf61353dfab4cb2e95fd7a8f8e7f12757efb7e0",
+          "message": "Making additional cmd line agruments available in the analysis script (#383)\n\n* Making additional command line agruments available in the analysis script\r\n\r\n* Providing all command line arguments\r\n\r\n* Adjusting batch example\r\n\r\n* .gitignore adjustment\r\n\r\n* Introduced muon_pt example argument\r\n\r\n* Providing additional args also to batch runs",
+          "timestamp": "2024-07-05T14:53:11+02:00",
+          "tree_id": "d2591be7e24118045899449a05123b48aa9ca01f",
+          "url": "https://github.com/amagnan/FCCAnalyses/commit/ecf61353dfab4cb2e95fd7a8f8e7f12757efb7e0"
+        },
+        "date": 1722248359399,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/weaver/analysis_inference.py",
+            "value": 32.83036017417908,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/weaver/analysis_inference.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 22.22024369239807,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: case-studies/analysis_example/scripts/analysis_example.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 12.785006046295166,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 14.264053583145142,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/test/jet_constituents.py",
+            "value": 12.032193899154663,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/test/jet_constituents.py"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 31.762353658676147,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      }
+    ]
+  }
+}
