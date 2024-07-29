433a5fc467f00149d6ab7982038b4c56c45cbe1b
github-action-benchmark <github@users.noreply.github.com>
2024-07-29 10:19:21 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for ecf61353dfab4cb2e95fd7a8f8e7f12757efb7e0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722248360178,
+  "lastUpdate": 1722248361515,
   "repoUrl": "https://github.com/amagnan/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -66,6 +66,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34742917+kjvbrt@users.noreply.github.com",
+            "name": "Juraj Smiesko",
+            "username": "kjvbrt"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ecf61353dfab4cb2e95fd7a8f8e7f12757efb7e0",
+          "message": "Making additional cmd line agruments available in the analysis script (#383)\n\n* Making additional command line agruments available in the analysis script\r\n\r\n* Providing all command line arguments\r\n\r\n* Adjusting batch example\r\n\r\n* .gitignore adjustment\r\n\r\n* Introduced muon_pt example argument\r\n\r\n* Providing additional args also to batch runs",
+          "timestamp": "2024-07-05T14:53:11+02:00",
+          "tree_id": "d2591be7e24118045899449a05123b48aa9ca01f",
+          "url": "https://github.com/amagnan/FCCAnalyses/commit/ecf61353dfab4cb2e95fd7a8f8e7f12757efb7e0"
+        },
+        "date": 1722248361034,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 3.0459611003186473,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 4.50040068796418,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.821662315832691,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 7.010629861778083,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 8.311036278016234,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.1483812904615833,
+            "unit": "Evt/s"
           }
         ]
       }
